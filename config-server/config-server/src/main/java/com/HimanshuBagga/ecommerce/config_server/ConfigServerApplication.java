f0c74788a8983b34a25eb9722f14cd54270bd59b
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-05-01 22:23:00 +0530

    Adding Centralized Configuration Server adding Auth

--- config-server/config-server/src/main/java/com/HimanshuBagga/ecommerce/config_server/ConfigServerApplication.java
+++ config-server/config-server/src/main/java/com/HimanshuBagga/ecommerce/config_server/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.HimanshuBagga.ecommerce.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer // starts config server by default
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
